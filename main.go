90e990263dc7141947280eed3314623cc383cf49
robertshagiev <shagiev173.robert@gmail.com>
2023-11-10 23:55:58 +0300

    1_task

--- main.go
+++ main.go
@@ -8,17 +8,10 @@ func main() {
 
 	intCh := make(chan int)
 
-	go factorial(5, intCh) // вызов горутины
-	fmt.Println(<-intCh)   // получение данных из канала
+	go func() {
 
-}
+		intCh <- 5 // отправка данных в канал
+	}() // вызов горутины
+	fmt.Println(<-intCh) // получение данных из канала
 
-func factorial(n int, ch chan int) {
-
-	result := 1
-	for i := 1; i <= n; i++ {
-		result += i
-	}
-
-	ch <- result // отправка данных в канал
 }
