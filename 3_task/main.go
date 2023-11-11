f4a11d297bd57c4b799b865c0c7f96769895e421
robertshagiev <shagiev173.robert@gmail.com>
2023-11-11 22:51:13 +0300

    repair1

--- 3_task/main.go
+++ 3_task/main.go
@@ -5,16 +5,18 @@ import (
 	"sync"
 )
 
-func square(in <-chan int, out chan<- int) { //функция для вычисления квадратов чисел
+// функция для вычисления квадратов чисел
+func write(in chan int, out chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
 	for val := range in { //интерация по каналу in, на каждой итерации текущий поток блокируется до следующего значения в канале
 		square := val * val
 		out <- square
 	}
 	close(out) // закрытие канала, новых значений поступать не будет. Запись в закрытый канал вызывавет панику
-	//закрываем для того, чтобы цикл по каналу прекратил считать значения
+
 }
 
-func read(out <-chan int, wg *sync.WaitGroup) { //считываение данных из канала out
+func read(out chan int, wg *sync.WaitGroup) { //считываение данных из канала out
 	defer wg.Done()
 	for val := range out { //интерация по каналу out и вывод его переменных
 		fmt.Println(val)
@@ -29,13 +31,16 @@ func main() {
 
 	a := [3]int{1, 2, 3}
 
-	go square(in, out) // горутниа которая возводит значения в квадрат канала in и передает их в канал out
-	wg.Add(1)
-	go read(out, &wg) // горунта которая выводит значения из канала out
-
 	for _, val := range a { // цикл который передает все значения в канал in
 		in <- val
 	}
+	wg.Add(1)
+	go write(in, out, &wg) // горутниа которая возводит значения в квадрат канала in и передает их в канал out
+
+	wg.Add(1)
+	go read(out, &wg) // горунта которая выводит значения из канала out
+
 	close(in)
 	wg.Wait()
+
 }
